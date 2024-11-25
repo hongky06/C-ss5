057569e1751ad09f27c8feadf82cd0b3750cea66
hongky06 <dohongky2006@gmail.com>
2024-11-25 21:08:30 +0700

    Add files via upload

---
+++ ss5 b5.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+
+
+for (int j = 1; j <= 9; j++) {
+        printf("bang cuu chuong so %d \n ",j);
+   for (int j = 1; j <= 9; j++){
+   } for (int i = 1 ; i < 10 ; i++ ){
+   	printf("%d x %d = %d \n",j,i,i*j);
+   }
+
+
+
+}
+   return 0;
+}
